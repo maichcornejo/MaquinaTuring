63f1311d743f1b02840ca92f15044e7933d4ee2f
Maia <maichcornejo@gmail.com>
2024-11-28 20:56:24 -0300

    primer commit

---
+++ frontend/cli/src/app/canvas/canvas.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-canvas',
+  imports: [],
+  template: `
+    <p>
+      canvas works!
+    </p>
+  `,
+  styles: ``
+})
+export class CanvasComponent {
+
+}
